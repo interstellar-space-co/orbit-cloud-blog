bd3e4607141e578f0ae8edb891374a3f419bd849
Mike Chase <themikechase@gmail.com>
2025-01-30 20:09:08 -0800

    new json

--- scripts/rss.mjs
+++ scripts/rss.mjs
@@ -3,9 +3,14 @@ import path from 'path'
 import { slug } from 'github-slugger'
 import { escape } from 'pliny/utils/htmlEscaper.js'
 import siteMetadata from '../data/siteMetadata.js'
-import tagData from '../app/tag-data.json' assert { type: 'json' }
 import { allBlogs } from '../.contentlayer/generated/index.mjs'
 import { sortPosts } from 'pliny/utils/contentlayer.js'
+import { readFileSync } from 'fs'
+
+
+const tagData = JSON.parse(
+  readFileSync(new URL('../app/tag-data.json', import.meta.url))
+)
 
 const generateRssItem = (config, post) => `
   <item>
